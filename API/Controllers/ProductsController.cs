480db4024881a89d5ab7295f342b9ee51b26bc0b
Achyut <achyutp10@gmail.com>
2025-03-26 19:49:34 +0545

    products controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,43 @@
-﻿namespace API.Controllers
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
 {
-    public class ProductsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProucts()
+        {
+            return await this.context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")] // api/products/2
+        public async Task<ActionResult<Product>> GetProuct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            return product;
+        }
+
+        [HttpPost()] 
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+            return Ok(product);
+        }
+
 
     }
 }
