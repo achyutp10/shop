ef6bd65d313e311cd463371cfd4ad75eb7e94dd1
Achyut <achyutp10@gmail.com>
2025-03-29 16:22:52 +0545

    added projection

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -42,6 +42,16 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public async Task<TResult?> GetEntityWithSpec<TResult>(ISpecification<T, TResult> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<TResult>> ListAsync<TResult>(ISpecification<T, TResult> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
         public void Remove(T entity)
         {
             context.Set<T>().Remove(entity);
@@ -61,6 +71,10 @@ namespace Infrastructure.Data
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(context.Set<T>().AsQueryable(), spec);
+        }
+        private IQueryable<TResult> ApplySpecification<TResult>(ISpecification<T, TResult> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery<T,TResult>(context.Set<T>().AsQueryable(), spec);
         }
     }
 }
