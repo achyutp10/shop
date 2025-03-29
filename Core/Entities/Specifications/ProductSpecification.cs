ead2ec1e648c71ff4287949e3a4624713fcd712b
Achyut <achyutp10@gmail.com>
2025-03-29 15:34:14 +0545

    added sorting to specification

--- Core/Entities/Specifications/ProductSpecification.cs
+++ Core/Entities/Specifications/ProductSpecification.cs
@@ -8,12 +8,23 @@ namespace Core.Entities.Specifications
 {
     public class ProductSpecification : BaseSpecification<Product>
     {
-        public ProductSpecification(string? brand, string? type) : base(x => 
+        public ProductSpecification(string? brand, string? type, string? sort) : base(x => 
          (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) &&
         (string.IsNullOrWhiteSpace(type) || x.Type == type)
         )
         {
-            
+            switch (sort)
+            {
+                case "priceAsc":
+                    AddOrderBy(x => x.Price);
+                    break;
+                case "priceDesc":
+                    AddOrderByDescending(x => x.Price);
+                    break;
+                default:
+                    AddOrderBy(x => x.Name);
+                    break;
+            }
         }
     }
 }
