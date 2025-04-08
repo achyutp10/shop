6439eafda503dd2e4dbbe8132cf7381b52ae4dc0
Achyut <achyutp10@gmail.com>
2025-04-08 13:43:41 +0545

    product details page

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -2,10 +2,24 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../../core/services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatInput, MatLabel } from '@angular/material/input';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    MatDivider,
+    MatIcon,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel
+  ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
